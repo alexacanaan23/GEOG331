a4136aad5a8f21ec6fe382f63fd96cd637d4a870
Alexa Canaan '20 <acanaan@colgate.edu>
2020-01-24 12:06:10 -0500

    Question 3 answers

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,2 +1,41 @@
 #Activity 1
 print("hello R world")
+
+#PRACTICE: mathematical operations
+#6 raised to the power of 6
+6^6
+
+#5 plus 210
+5+210
+
+#3 minus 10
+3-10
+
+#PRACTICE: naming and assigning values
+#name a number, will show up in environment
+a<- 2446
+
+#multiply my number by 5
+5*a
+
+#divide my number by 2
+a/2
+
+
+#PRACTICE: vectors
+#make a vector
+b <- c(2446,40,1000)
+
+#divide all numbers in the vector by 2
+#this doesn't change the values in the vector
+b/2
+
+#QUESTION 3
+#create a vector
+myvector<- c(240, 23, 89, 74, 65)
+#calculation 1: multiply my vector by 3
+myvector*3
+#calculation 2: divide my vector by 14
+myvector/14
+#calculation 3: add 23 to all values in my vector
+myvector+23
