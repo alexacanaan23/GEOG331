62bed71a348a50145c966b6c3523a96d2b707162
Alexa Canaan '20 <acanaan@colgate.edu>
2020-03-06 12:07:12 -0500

    activity 6 setup

--- activity6/activity6_script.R
+++ activity6/activity6_script.R
@@ -15,10 +15,13 @@ library(plyr)
 #read in shapefiles
 #readOGR in rgdal does this
 g1966 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_1966.shp")
+plot(g1966,axes="TRUE")
 g1998 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_1998.shp")
+plot(g1998,axes="TRUE")
 g2005 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_2005.shp")
+plot(g2005,axes="TRUE")
 g2015 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_2015.shp")
-
+plot(g2015,axes="TRUE")
 str(g2015)
 
 #data stores all accompanying info/measurements for each spatial object
@@ -29,5 +32,8 @@ g2015@polygons[[1]]
 
 #vector object
 g1966@proj4string
+
+#get polygon coordinates
+g1966@polygons[[1]]@Polygons[[1]]
 
 #QUESTION 1 - NO CODE
